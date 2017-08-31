4f5c4befb92345c1d65ca4efb889fd9e7a52a48d
Marty M <martin.mooradian@gmail.com>
2017-08-31 22:27:54 +0000

    Edited controller and added Show feature.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,16 +4,26 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
   end
   
+  def show
+    @contact = Contact.find(params[:id])
+  end
+  
   def new
     @contact = Contact.new
   end
   
   def create
-   # render plain: params[:contact].inspect
-    @contact = Contact.new(contact_params)
-    @contact.save
     
-    redirect_to contacts_show(@contact)
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:notice] = "Contact was successfully created."
+      redirect_to contact_path(@contact)
+      
+    else
+      
+      render 'new'
+    end
   end
   
   private
