c328908d41b96658fbffdf4621b33e1885c593b7
Marty M <martin.mooradian@gmail.com>
2017-08-31 15:23:43 +0000

    Updated Validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
-  
+  validates :last_name, presence: true, length: {minimum: 2, maximum: 15}
+  validates :first_name, presence: true, length: {minimum: 2, maximum: 15}
   
 end
